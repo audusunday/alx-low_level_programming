008b437a237a68a01f908c9ea036aa600ce52f45
audusunday <sunnadu13@gmail.com>
2022-10-10 21:50:05 +0000

    fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * intit_dog - initializes a variable of type struct dog.
+ * init_dog - initializes a variable of type struct dog.
  * @d: pointer to structure.
  * @name: first member.
  * @age: second member.
