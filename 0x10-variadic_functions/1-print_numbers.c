88ff53356d4632116ac649086f5add2c96fbe023
audusunday <sunnadu13@gmail.com>
2022-10-13 20:17:58 +0000

    commit for task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,10 +12,10 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum;
-	va_list vallist;
+	int num;
+	va_list valist;
 
-	va_start(list, n);
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(valist, int);
@@ -24,6 +24,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 	}
 	printf("\n");
+	va_end(valist);
 }
-
-
